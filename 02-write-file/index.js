0e21bb7dfc4fba84070d8fc70388b195cea1e095
alima987 <alima1996b@gmail.com>
2023-05-05 22:21:40 +0500

    feat: finish 02 task

---
+++ 02-write-file/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const readline = require('readline');
+const fileStream = fs.createWriteStream('input.txt', { flags: 'a' });
+console.log('Введите текст. Для выхода введите "exit" или нажмите Ctrl + C\n');
+const rl = readline.createInterface({
+input: process.stdin,
+output: process.stdout,
+prompt: '',
+});
+
+rl.prompt();
+
+rl.on('line', (line) => {
+
+if (line.trim().toLowerCase() === 'exit') {
+
+console.log('Программа завершена');
+process.exit(0);
+}
+
+
+fileStream.write(`${line}\n`);
+
+rl.prompt();
+}).on('close', () => {
+
+console.log('Программа завершена');
+});
+
