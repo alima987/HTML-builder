6713ab5877ee52c1eb0302d59d1266f164088eb9
alima987 <alima1996b@gmail.com>
2023-05-05 22:43:44 +0500

    feat: finish 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const dir = path.join(__dirname, 'files-copy');
+if (!fs.existsSync(dir)) {
+fs.mkdirSync(dir);
+}
 
+const filesDir = path.join(__dirname, 'files');
+fs.readdir(filesDir, (err, files) => {
+if (err) throw err;
+
+files.forEach(file => {
+const srcFile = path.join(filesDir, file);
+const destFile = path.join(dir, file);
+fs.copyFileSync(srcFile, destFile);
+});
+});
