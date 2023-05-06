817c64987baf844f3f12216c2c87c97e8b9a42be
alima987 <alima1996b@gmail.com>
2023-05-06 17:57:19 +0500

    fix: fix 04 file according to eslint

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,16 +2,16 @@ const fs = require('fs');
 const path = require('path');
 const dir = path.join(__dirname, 'files-copy');
 if (!fs.existsSync(dir)) {
-fs.mkdirSync(dir);
+  fs.mkdirSync(dir);
 }
 
 const filesDir = path.join(__dirname, 'files');
 fs.readdir(filesDir, (err, files) => {
-if (err) throw err;
+  if (err) throw err;
 
-files.forEach(file => {
-const srcFile = path.join(filesDir, file);
-const destFile = path.join(dir, file);
-fs.copyFileSync(srcFile, destFile);
-});
+  files.forEach(file => {
+    const srcFile = path.join(filesDir, file);
+    const destFile = path.join(dir, file);
+    fs.copyFileSync(srcFile, destFile);
+  });
 });
