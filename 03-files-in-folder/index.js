6fe502a4cf39ac6fc39b92cce9244c179ef65731
alima987 <alima1996b@gmail.com>
2023-05-06 17:53:39 +0500

    fix: fix 03 file according to eslint

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,23 +4,23 @@ const path = require('node:path');
 const folderPath = path.join(__dirname, 'secret-folder');
 
 fs.readdir(folderPath, (err, files) => {
-if (err) {
-console.error(err);
-return;
-}
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-files.forEach(file => {
-const filePath = path.join(folderPath, file);
+  files.forEach(file => {
+    const filePath = path.join(folderPath, file);
 
-fs.stat(filePath, (err, stats) => {
-if (err) {
-console.error(err);
-return;
-}
+    fs.stat(filePath, (err, stats) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
 
-if (stats.isFile()) {
-console.log(`${file} - ${stats.size} bytes`);
-}
-});
-});
+      if (stats.isFile()) {
+        console.log(`${file} - ${stats.size} bytes`);
+      }
+    });
+  });
 });
