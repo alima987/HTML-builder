7ba7429ec2137a3280a47c5d8010a8a87c52e459
alima987 <alima1996b@gmail.com>
2023-05-11 11:59:07 +0500

    fix: fix 01 file add readStream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,7 +2,11 @@ const fs = require('fs');
 const path = require('node:path');
 
 const pathToText = path.join(__dirname, 'text.txt');
-fs.readFile(pathToText, 'utf-8', (error, data) => {
-  if (error) throw error;
-  else console.log(data);
-}); 
+const readStream = fs.createReadStream(pathToText, 'utf-8');
+readStream.on('data', (chunk) => {
+  console.log(chunk);
+});
+  
+readStream.on('error', (error) => {
+  throw error;
+});
